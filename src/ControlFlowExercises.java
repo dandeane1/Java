4b8466d4db4105162625da88d2442ce025f1db71
Daniel Deane <daniel.michael.deane@gmail.com>
2023-10-05 19:55:29 -0500

    still need to work on the last one runs but only when the one above it is committed out

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,6 @@
 import java.util.Enumeration;
 import java.util.Scanner;
+
 public class ControlFlowExercises {
     //    Create a while loop that runs so long as i is less than or equal to 15
     public static void main(String[] args) {
@@ -62,84 +63,82 @@ public class ControlFlowExercises {
             }
 
 
+            Scanner scanner = new Scanner(System.in);
+            String choice;
 
-                Scanner scanner = new Scanner(System.in);
-                String choice;
+            do {
+                System.out.print("Enter an integer: ");
+                int n = scanner.nextInt();
 
-                do {
-                    System.out.print("Enter an integer: ");
-                    int n = scanner.nextInt();
+                System.out.println("number\tsquared\tcubed");
+                System.out.println("------\t------\t------");
 
-                    System.out.println("number\tsquared\tcubed");
-                    System.out.println("------\t------\t------");
+                for (int i = 1; i <= n; i++) {
+                    int square = i * i;
+                    int cube = i * i * i;
+                    System.out.printf("%5d\t%6d\t%5d%n", i, square, cube);
+                }
 
-                    for (int i = 1; i <= n; i++) {
-                        int square = i * i;
-                        int cube = i * i * i;
-                        System.out.printf("%5d\t%6d\t%5d%n", i, square, cube);
-                    }
+                System.out.print("Do you want to continue (yes/no)? ");
+                scanner.nextLine();
+                choice = scanner.nextLine();
+            } while (choice.equalsIgnoreCase("yes"));
 
-                    System.out.print("Do you want to continue (yes/no)? ");
-                    scanner.nextLine();
-                    choice = scanner.nextLine();
-                } while (choice.equalsIgnoreCase("yes"));
+        }
 
+
+        Scanner input = new Scanner(System.in);
+        String choice;
+
+        do {
+            System.out.print("Enter a numerical grade (0-100): "); // prompt the user for a numerical grade
+            int numericalGrade = input.nextInt(); // read the numerical grade as an integer
+
+            String letterGrade; // declare a variable to store the letter grade
+
+            // use a switch statement to assign the letter grade based on the first digit of the numerical grade
+            switch (numericalGrade / 10) {
+                case 10:
+                case 9:
+                    letterGrade = "A";
+                    break;
+                case 8:
+                    letterGrade = "B";
+                    break;
+                case 7:
+                    letterGrade = "C";
+                    break;
+                case 6:
+                    letterGrade = "D";
+                    break;
+                default:
+                    letterGrade = "F";
             }
 
-
-
-                Scanner input = new Scanner(System.in);
-                String choice;
-
-                do {
-                    System.out.print("Enter a numerical grade (0-100): "); // prompt the user for a numerical grade
-                    int numericalGrade = input.nextInt(); // read the numerical grade as an integer
-
-                    String letterGrade; // declare a variable to store the letter grade
-
-                    // use a switch statement to assign the letter grade based on the first digit of the numerical grade
-                    switch (numericalGrade / 10) {
-                        case 10:
-                        case 9:
-                            letterGrade = "A";
-                            break;
-                        case 8:
-                            letterGrade = "B";
-                            break;
-                        case 7:
-                            letterGrade = "C";
-                            break;
-                        case 6:
-                            letterGrade = "D";
-                            break;
-                        default:
-                            letterGrade = "F";
+            // use a nested switch statement to add a plus or minus sign based on the second digit of the numerical grade
+            switch (numericalGrade % 10) {
+                case 9:
+                case 8:
+                    if (!letterGrade.equals("F")) { // do not add a plus sign for F grades
+                        letterGrade += "+";
                     }
-
-                    // use a nested switch statement to add a plus or minus sign based on the second digit of the numerical grade
-                    switch (numericalGrade % 10) {
-                        case 9:
-                        case 8:
-                            if (!letterGrade.equals("F")) { // do not add a plus sign for F grades
-                                letterGrade += "+";
-                            }
-                            break;
-                        case 1:
-                        case 0:
-                            if (!letterGrade.equals("F")) { // do not add a minus sign for F grades
-                                letterGrade += "-";
-                            }
-                            break;
+                    break;
+                case 1:
+                case 0:
+                    if (!letterGrade.equals("F")) { // do not add a minus sign for F grades
+                        letterGrade += "-";
                     }
+                    break;
+            }
 
-                    // print the letter grade
-                    System.out.println("The letter grade is: " + letterGrade);
+            // print the letter grade
+            System.out.println("The letter grade is: " + letterGrade);
 
-                    // ask the user if they want to continue
-                    System.out.print("Do you want to continue (yes/no)? ");
-                    input.nextLine(); // consume the newline character
-                    choice = input.nextLine(); // read the user's choice as a string
-                } while (choice.equalsIgnoreCase("yes")); // repeat the loop if the user says yes
+            // ask the user if they want to continue
+            System.out.print("Do you want to continue (yes/no)? ");
+            input.nextLine(); // consume the newline character
+            choice = input.nextLine(); // read the user's choice as a string
+        } while (choice.equalsIgnoreCase("yes")); // repeat the loop if the user says yes
 
                 input.close(); // close the scanner object
             }
