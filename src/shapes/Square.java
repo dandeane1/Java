bf7646a78cbcaf1e2a45775a657f5cc2bffcf49d
Daniel Deane <daniel.michael.deane@gmail.com>
2023-10-09 12:25:51 -0500

    shapes complete

---
+++ src/shapes/Square.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * length;
+    }
+
+    @Override
+    public double getArea() {
+        return length * length;
+    }
+}
